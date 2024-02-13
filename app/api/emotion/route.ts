944ecfedb198279a74ce7ac9f64d33fa7c5168bd
laguagu <49702497+laguagu@users.noreply.github.com>
2024-02-13 18:40:41 +0200

    feat: Show user emotion based on text input

--- app/api/emotion/route.ts
+++ app/api/emotion/route.ts
@@ -1,14 +1,24 @@
 import { HfInference, TextClassificationOutput } from "@huggingface/inference";
+import { headers } from "next/headers";
 const hf = new HfInference(process.env.HUGGIGN_FACE);
 
 export async function POST(request: Request, response: Response) {
   const { input } = await request.json();
   const inferenceResult: TextClassificationOutput = await inference(input);
-  const filterEmotions = await filteredEmotion([...inferenceResult])
-  console.log(inferenceResult);
-  console.log(filterEmotions);
+  const filterEmotions = await filteredEmotion([...inferenceResult]);
 
-  return new Response("Moikka", { status: 200 });
+  return new Response(
+    JSON.stringify({
+      inferenceResult,
+      filterEmotions,
+    }),
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 }
 
 async function inference(input: string) {
@@ -20,8 +30,8 @@ async function inference(input: string) {
   return hfresult;
 }
 
-async function filteredEmotion(emotions:TextClassificationOutput) {
-    if (!emotions || emotions.length === 0) return [];
-    const firstEmotionScore = emotions[0].score;
-    return emotions.filter(emotion => emotion.score >= firstEmotionScore * 0.5);
+async function filteredEmotion(emotions: TextClassificationOutput) {
+  if (!emotions || emotions.length === 0) return [];
+  const firstEmotionScore = emotions[0].score;
+  return emotions.filter((emotion) => emotion.score >= firstEmotionScore * 0.5);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
+import { emotionStyles } from "./emotions";
 import { useDebouncedCallback } from "use-debounce";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Home() {
   const [input, setInput] = useState<string>("");
+  const [emoji, setEmoji] = useState({ label: "", score: 0 });
+  const [style, setStyle] = useState({emoji: "", bgColor: "bg-white"})
 
   const handleInputChange = useDebouncedCallback(async (value) => {
     setInput(value);
@@ -13,12 +16,16 @@ export default function Home() {
     }
   }, 1000);
 
+
   async function runPrediction(input: string) {
     const result = await axios.post("api/emotion", { input });
+    const detectedEmotion = result.data.filterEmotions[0]
+    setEmoji(detectedEmotion)
+    updateUIbasedOnEmotion(detectedEmotion.label)
   }
 
   return (
-    <main className="flex items-center flex-col p-24 min-h-screen gap-4">
+<main className={`flex items-center flex-col p-24 min-h-screen gap-4 ${style.bgColor}`}>
       <h1 className="text-2xl ">🧑‍🎨 Show my mood 🧹</h1>
       <div className="w-full max-w-lg px-4">
         <textarea
@@ -28,6 +35,18 @@ export default function Home() {
         ></textarea>
       </div>
       <p>{">" + input}</p>
+      <div>
+        <span>You feel {emoji.label}{style.emoji}</span>
+      </div>
     </main>
   );
-}
+  type EmotionLabel = keyof typeof emotionStyles;
+  
+  function updateUIbasedOnEmotion(label: EmotionLabel) {
+      const emotionStyle = emotionStyles[label] || { emoji: "❓", bgColor: "bg-white" };
+      setStyle(emotionStyle)
+      console.log(style);
+      
+  }
+
+}  
